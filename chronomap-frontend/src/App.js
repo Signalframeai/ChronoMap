9e7e2a2c84ea1fe7a2873085b49dafabde46e73e
Signalframeai <stephenknowles1209@gmail.com>
2025-04-04 21:31:09 +0100

    set up React app with basic Hello World page

---
+++ chronomap-frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to ChronoMap!</h1>
+      <p>Explore history through AI-powered reconstructions.</p>
+    </div>
+  );
+}
+
+export default App;
